1596f8b84a948f4b202ccc1dc60c83e5432df936
YuraVasko <yuravasko2016@gmail.com>
2020-07-10 18:30:35 +0300

    Add DAL basics

--- TransactionDataService/Transaction.Data.Service.DAL/Interfaces/ITransactionDataRepository.cs
+++ TransactionDataService/Transaction.Data.Service.DAL/Interfaces/ITransactionDataRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace Transaction.Data.Service.DAL.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Transaction.Data.Service.DAL.Models;
+
+namespace Transaction.Data.Service.DAL.Interfaces
 {
     public interface ITransactionDataRepository
     {
+        Task<IReadOnlyCollection<TransactionData>> GetTransactionDataAsync();
+
+        Task InsertAsync(TransactionData transactionData);
+
+        Task InsertRangeAsync(IEnumerable<TransactionData> transactionData);
+
+        Task DeleteAsync(string id);
     }
 }
---
+++ TransactionDataService/Transaction.Data.Service.DAL/TransactionDataServiceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Transaction.Data.Service.DAL.Models;
+
+namespace Transaction.Data.Service.DAL
+{
+    public class TransactionDataServiceDbContext : DbContext
+    {
+        public DbSet<TransactionData> TransactionData { get; set; }
+
+        public TransactionDataServiceDbContext(DbContextOptions<TransactionDataServiceDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
